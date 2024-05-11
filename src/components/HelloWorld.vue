4af37adfdc10b7905bd0aaff666e2891fd2d2e26
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-05-11 18:22:50 +0600

    chore: Update dependencies and import paths

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="text-green-600 text-3xl font-semibold">{{ msg }}</h1>
     <h3>
       You&apos;ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
