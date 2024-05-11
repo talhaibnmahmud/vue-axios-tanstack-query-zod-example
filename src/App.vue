c83b1061519695e9627078b18f2beab68ae1a413
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-05-11 19:08:53 +0600

    Update import paths and add useFetcher composable

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
+import { storeToRefs } from 'pinia'
+import { onMounted } from 'vue'
 
 import HelloWorld from '@/components/HelloWorld.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+const { demoLogin } = authStore
+const { auth } = storeToRefs(authStore)
+console.log(auth.value)
+
+onMounted(() => {
+  demoLogin()
+})
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,8 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
-
 import { useFetcher } from '@/composables/useFetcher'
-import { useAuthStore } from '@/stores/auth'
 import type { Post } from '@/schemas/post.schema'
 
-const authStore = useAuthStore()
-const { demoLogin } = authStore
-const { auth } = storeToRefs(authStore)
-console.log(auth.value)
-
 const { data, error, isLoading, refetch } = useFetcher<Post[]>(['posts'], '/posts')
-
-onMounted(() => {
-  demoLogin()
-})
 </script>
 
 <template>
