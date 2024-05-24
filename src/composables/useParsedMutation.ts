b1b1f234ee94ed01f29f4778b0cf8f4512727120
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-05-25 02:45:43 +0600

    refactor: Update useParsedMutation to support different HTTP methods

--- src/composables/useParsedMutation.ts
+++ src/composables/useParsedMutation.ts
@@ -4,26 +4,40 @@ import { z } from 'zod'
 
 import { mutator } from '@/utils/mutator'
 
-type FetchOptions<Z extends z.ZodTypeAny, D = any> = {
+type MutationOptions<Z extends z.ZodTypeAny> = {
   key: QueryKey
-  url: string
-  body?: D
   schema?: Z
   useAuth?: boolean
   options?: AxiosRequestConfig
 }
 
-export const useParsedMutation = <D = any, Z extends z.ZodTypeAny = z.ZodNever, R = z.infer<Z>>({
+type TVariables<TBody> = { url: string; body?: TBody }
+
+export const useParsedMutation = <
+  TBody = void,
+  Z extends z.ZodTypeAny = z.ZodNever,
+  TData = z.infer<Z>
+>({
   key,
-  url,
-  body,
   schema,
   useAuth = true,
   options = { method: 'POST' }
-}: FetchOptions<Z, D>) => {
-  const mutation = useMutation<R, Error, D, [string, string, D, Z, boolean, AxiosRequestConfig]>({
-    mutationKey: [key, url, body, schema, useAuth, options],
-    mutationFn: (variables) => mutator<D, Z, R>({ url, body: variables, schema, useAuth, options })
+}: MutationOptions<Z>) => {
+  const mutation = useMutation<
+    TData,
+    Error,
+    TVariables<TBody>
+    // { key: QueryKey; url: string; body: TVariables; schema?: Z; useAuth?: boolean; options?: AxiosRequestConfig }
+  >({
+    mutationKey: [key, schema, useAuth, options],
+    mutationFn: (variables) =>
+      mutator<TVariables<TBody>['body'], Z, TData>({
+        url: variables.url,
+        body: variables.body,
+        schema,
+        useAuth,
+        options
+      })
   })
 
   return mutation
